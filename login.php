5bfa0121b3d1f18e0651e278f5cce0b7a680c2ca
nisselpipe <50411209+nisselpipe@users.noreply.github.com>
2019-05-21 14:09:14 +0200

    Fixat lite

--- login.php
+++ login.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css.css">
-</head>
-
-<body>
-    <form> 
-        Användarnamn: <br>
-        <input type="text" name="userName"> <br>
-        Lösenord: <br>
-        <input type="text" name="password"> <br>
-    </form>
+<?php
 
 
-<script>
-
-
-</script>
-</body>
-
-
-</html>
+?>
