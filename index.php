3b8868cbfd95d26bf079726d1855ce6eb38d3efc
DDaaNNee <50401918+DDaaNNee@users.noreply.github.com>
2019-05-21 12:29:29 +0200

    Lagt in ikon och fixat med placeringen av topbar

--- index.php
+++ index.php
@@ -8,25 +8,25 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   </head>
   <body>
-    <h10>
+    <?php require 'process.php'; ?>
+    <h5>
     <?php
-    include 'header.php';
-    include 'process.php';
-
-    echo "Uppkopplad! <br>";
 
     $sql = "SELECT ID, Fornamn, Efternamn, Email, Telefonnummer FROM Users";
     $result = $conn->query($sql);
 
+    echo "<table border='1'>";
+    echo "<tr><td>ID</td><td>Förnamn</td><td>Efternamn</td><td>E-mail</td><td>Telefonnummer</td><tr>\n";
+
     if ($result->num_rows > 0) {
       while ($row = $result -> fetch_assoc()) {
-        echo "<br> ID: " . $row["ID"]. "<br>Förnamn: " . $row["Fornamn"] . "<br>Efternamn: " . $row["Efternamn"] . "<br>E-mail: " . $row["Email"] ."<br>Telefonnummer: " . $row["Telefonnummer"] . "<br>";
+        echo "<tr><td>{$row["ID"]}</td><td>{$row["Fornamn"]}</td><td>{$row["Efternamn"]}</td><td>{$row["Email"]}</td><td>{$row["Telefonnummer"]}</td><tr>";
       }
     }
     else {
         echo "0 results";
     }
     ?>
-  </h10>
+  </h5>
   </body>
 </html>
