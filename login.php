9c1fbff8a3797733c1a14d7b93db4e4fdef888dd
VVilgot <50405993+VVilgot@users.noreply.github.com>
2019-05-21 14:17:35 +0200

    Fixelishes

--- login.php
+++ login.php
@@ -1,4 +1,22 @@
 <?php
+$dbServer = "dbtrain.im.uu.se";
+$username = "dbtrain_890";
+$password = "jrylmp";
+$dbname = "dbtrain_890";
+
+$conn = new mysqli($dbServer, $username, $password, $dbname);
+
+if (!$conn) {
+     die("Det gick inte att ansluta! Error: ". mysqli_connect_error());
+}
+
+echo "Uppkopplad! <br>";
+
+$fornamn = $_POST['fornamn'];
+$efternamn = $_POST['efternamn'];
+$email = $_POST['email'];
+$telefonnummer = $_POST['mobilnummer'];
+
 
 
 ?>
