e6edca65b9c36e76a60ad5bf58405bb9a8feb041
nisselpipe <50411209+nisselpipe@users.noreply.github.com>
2019-05-29 14:50:17 +0200

    Lägga till paket i databas
    Skapade även en databas

--- products.php
+++ products.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -14,23 +15,36 @@
      <!--     <img src="" alt="Gamingpaketet">  Här ska vi lägga till bild-->
         <form method="POST">
             <input type="number" name="gamingpaket" required>
-            <input type="submit" name="submitGaming">
+            <input type="submit" name="submitGaming" required>
         </form>
 
      <!--   <img src="" alt="Träningspaketet">  Här ska vi lägga till bild-->
         <form method="POST">
-            <input type="number" name="sportpaketet" required>
-            <input type="submit" name="submitSport">
+            <input type="number" name="sportpaket" required>
+            <input type="submit" name="submitSport" required>
         </form>
 
       <!--    <img src="" alt="Extremsportspaketet"> Här ska vi lägga till bild-->
         <form method="POST">
-            <input type="number" name="extremsport" required>
-            <input type="submit" name="submitExtrem">
+            <input type="number" name="extremsportpaket" required>
+            <input type="submit" name="submitExtrem" required>
         </form>
 
     <?php
+        //Metod för att lägga till antal gamingpaket i databasen
+        $antalGaming = mysqli_real_escape_string($_POST['gamingpaket']);
+        $gamingQuery = "INSERT INTO Beställningar(Gamingpaket) VALUES ($antalGaming)";
+        $addGaming=mysqli_query($connection, $gamingQuery);
 
+        //Metod för att lägga till antal sportpaket i databasen
+        $antalSport = mysqli_real_escape_string($_POST['sportpaket']);
+        $sportQuery = "INSERT INTO Beställningar(Sportpaket) VALUES ($antalSport)";
+        $addSport=mysqli_query($connection, $sportQuery);
+
+        //Metod för att lägga till antal extrempaket i databasen
+        $antalExtrem = mysqli_real_escape_string($_POST['extremsportpaket']);
+        $extremQuery = "INSERT INTO Beställningar(Extrempaket) VALUES ($antalExtrem)";
+        $addSport=mysqli_query($connection, $extremQuery);
     ?>
 
     </body>
