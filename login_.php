d4773e581e8822b44e7966b00138444ae22414cb
nisselpipe <50411209+nisselpipe@users.noreply.github.com>
2019-05-29 15:37:52 +0200

    Login som funkar

--- login_.php
+++ login_.php
@@ -1,49 +1,28 @@
 <?php
-$dbServer = "dbtrain.im.uu.se";
-$username = "dbtrain_890";
-$password = "jrylmp";
-$dbname = "dbtrain_890";
+include 'dbconnect.php';
 
-$conn = new mysqli($dbServer, $username, $password, $dbname);
 
-if (!$conn) {
-     die("Det gick inte att ansluta! Error: ". mysqli_connect_error());
-}
+//If-sats här för att det inte ska visa att det inte är ifyllt på hemsidan
+if(!empty($_POST('username')){
+    $loginEmail = mysqli_real_escape_string($_POST['username']);
+    $loginPassword = mysqli_real_escape_string($_POST['userPassword'])
 
-echo "Uppkopplad! <br>";
+    $findHashPassword="SELECT Losenord FROM Anvandare WHERE Email='$loginEmail'";
+    $hashQuerySQL= mysqli_query($conn, $findHashPassword) or die(mysqli_error($conn));
+    $findPasswordRow= mysqli_fetch_row($resultPass);
+    $findPasswordRow= $findPasswordRow[0];
 
-$userName = mysqli_real_escape_string($_POST['userName']);
-$password = mysqli_real_escape_string($_POST['userPassword']);
+    if(password_verify($loginPassword, $findPasswordRow){
+        $_SESSION['username']=$NameUser;
+        echo"<script>location.href='index.php'</script>";
+    }
+    else{
+        echo "Användarnamnet eller lösenordet är felaktigt"
+    }
 
-$validation = "INSERT INTO TempUser('tempUser', 'tempPassword') VALUES ('$userName', 'userPassword')";
-
-$selectQuery= "SELECT 'Email' AND 'Password' FROM 'Users'";
-//Under denna rad är det lite otydligt hur vi exakt ska göra, vi får eventuellt fråga Stuxberg
-//då vi måste använda oss av saltning och kan inte spara lösenorden i "plain text".
-$validating = if($validation==$selectQuery){
-return true;
-}
-else {
-    return false;
-}
-
-if($validation==true){
-    //Här ska vi lägga in logg in information, vet inte riktigt hur vi ska göra det
-}
-else{
-    echo "Fel användarnamn eller lösenord";
-}
-
-/*
-if (mysqli_query($conn, $validation) {
-    echo "Adderad information till databasen! <br>";
-}
-else {
-    echo "ERROR: " . $sql . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
-*/
 /*header('Location: index.php'); */
 
 ?>
